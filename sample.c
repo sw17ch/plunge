588cfdac8cd9e08e210a88c48808cab9538d340f
John Van Enk <vanenkj@gmail.com>
2012-10-01 22:50:09 -0400

    Let's add some stuff that will not show up as anything in the CPP output.

--- sample.c
+++ sample.c
@@ -2,7 +2,19 @@
 #include "foo.h"
 #include "bar.h"
 
+#define A_DEFN(x) \
+  struct a_struct_named_##x { \
+    int int_##x; \
+    float float_##x; \
+  };
+
+/*
+ * This is a long comment. It will be stripped in the CPP output. It's good to
+ * include things like this in test files.
+ */
+
 int foo(int x) {
+  A_DEFN(neato);
   return ADD_THINGS(x, 5);
 }
 
