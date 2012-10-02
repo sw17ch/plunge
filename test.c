c67c1090996cf350481ef337011208609be22ab2
John Van Enk <vanenkj@gmail.com>
2012-10-01 22:50:22 -0400

    Let's add some stuff that will not show up as anything in the CPP output.

--- test.c
+++ test.c
@@ -2,6 +2,11 @@
 #include "foo.h"
 #include "bar.h"
 
+/*
+ * This is a long comment. It will be stripped in the CPP output. It's good to
+ * include things like this in test files.
+ */
+
 int main(int argc, char * argv[]) {
   printf("Hello World!\n");
   return 0;
