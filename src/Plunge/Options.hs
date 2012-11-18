66e9ac4429211d9ae390e53b63cf6333467fbfcf
John Van Enk <vanenkj@gmail.com>
2012-11-18 14:13:25 -0500

    Better names/descriptions.

--- src/Plunge/Options.hs
+++ src/Plunge/Options.hs
@@ -22,16 +22,24 @@ data Options = Options { inputFile    :: FilePath
                        } deriving (Show, Data, Typeable)
 
 defaultOpts :: Options
-defaultOpts = Options { inputFile = def      &= name "input-file"
-                      , gccOptions = def     &= name "gcc-option"
+defaultOpts = Options { inputFile = def      &= explicit &= name "input-file"
+                                                         &= name "i"
+                                             &= inputFile_help
+                      , gccOptions = def     &= explicit &= name "gcc-option"
+                                                         &= name "g"
                                              &= gccOptions_help
-                      , linePadder = " "     &= name "line-padding"
-                      , emptyLine = "."      &= name "empty-line-padding"
-                      , maxWidth = Nothing   &= name "max-width"
+                      , linePadder = " "     &= explicit &= name "line-padding"
+                                                         &= name "p"
+                      , emptyLine = "."      &= explicit &= name "empty-line-padding"
+                                                         &= name "e"
+                      , maxWidth = Nothing   &= explicit &= name "max-width"
+                                                         &= name "w"
                       -- , showLineNums = False &= name "show-line-numbers"
                       -- , colorize = False     &= name "colorize"
-                      , verticalSep = " | "  &= name "vertical-sep"
-                      , horizSep = "-"       &= name "horizontal-sep"
+                      , verticalSep = " | "  &= explicit &= name "vertical-sep"
+                                                         &= name "v"
+                      , horizSep = "-"       &= explicit &= name "horizontal-sep"
+                                                         &= name "h"
                       } &= program "plunge"
                         &= summary summary_str
 
@@ -43,6 +51,10 @@ summary_str = let tags = versionTags version
                                 [] -> ""
                                 _  -> " (" ++ (concat $ intersperse ", " $ tags) ++ ")"
             in "Plunge " ++ branch_str ++ tags_str ++ ", (C) John Van Enk 2012"
+
+
+inputFile_help :: Ann
+inputFile_help = help "The C file to analyze."
 
 gccOptions_help :: Ann
 gccOptions_help = help "An option to pass to GCC. Can be specified multiple times."
